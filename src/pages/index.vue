e4291d29b65d98ca5e5ded7c363467d8d4467b95
ap-19927 <allmannpatrick@gmail.com>
2023-12-13 09:01:25 +0000

    display map with vue3-openlayers

---
+++ src/pages/index.vue
@@ -0,0 +1,32 @@
+<script setup>
+//https://vue3openlayers.netlify.app/componentsguide/map/#usage
+import { ref } from "vue";
+
+const center = ref([40, 40]);
+const projection = ref("EPSG:4326");
+const zoom = ref(8);
+const rotation = ref(0);
+</script>
+
+<template>
+  <ol-map
+    :loadTilesWhileAnimating="true"
+    :loadTilesWhileInteracting="true"
+    style="height: 400vh"
+  >
+    <ol-view
+      ref="view"
+      :center="center"
+      :rotation="rotation"
+      :zoom="zoom"
+      :projection="projection"
+    />
+
+    <ol-tile-layer>
+      <ol-source-osm />
+    </ol-tile-layer>
+  </ol-map>
+</template>
+<style scoped>
+@import 'vue3-openlayers/dist/vue3-openlayers.css';
+</style>
---
+++ src/plugins/vue3-openlayers.client.ts
@@ -0,0 +1,8 @@
+// https://stackoverflow.com/a/70193023
+// plugins/vue3-openlayers.client.js
+import { defineNuxtPlugin } from '#app'
+import OpenLayers from 'vue3-openlayers'
+
+export default defineNuxtPlugin(nuxtApp => {
+  nuxtApp.vueApp.use(OpenLayers)
+})
