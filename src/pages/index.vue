e9c257d49c562f26c0c2095d3a499c7aea96ff04
ap-19927 <allmannpatrick@gmail.com>
2024-01-05 19:56:34 +0000

    graphHopper to write polyLine along tile

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,9 +3,9 @@
 import { ref } from "vue";
 import Kompas from "kompas";
 
-const center = ref([40, 40]);
-const projection = ref("EPSG:3857");
-const zoom = ref(8);
+const center = ref([-117,33]);
+const projection = ref("EPSG:4326");
+const zoom = ref(9);
 const rotation = ref(0);
 const hereIcon = "https://raw.githubusercontent.com/ap-19927/rp/main/map/data/location-heading.svg";
 
@@ -46,6 +46,46 @@ function error(err) {
 }
 
 navigator.geolocation.getCurrentPosition(success, error, options);
+const count = ref(0)
+
+const y1 = 33.47
+const y2 = 33 
+const x1 = -117
+const x2 = -117.0911645
+const key = "d226e6ab-bff6-4e39-a97a-3ee7c3259710"
+const ghURL = `https://graphhopper.com/api/1/route?point=${y1},${x1}&point=${y2},${x2}&points_encoded=false&key=${key}`
+let polyline = await useFetch(ghURL)
+polyline = polyline.data._rawValue.paths[0].points.coordinates;
+const coordinates = ref(polyline);
+console.log("POINTS:",polyline)
+const strokeWidth = ref(6);
+const strokeColor = ref([0,0,0,0.8]);
+
+const selectConditions = inject("ol-selectconditions");
+
+const selectCondition = selectConditions.click;
+
+const m = 0;
+const featureSelected = (event) => {
+  console.log("FEATURE:",event);
+/*
+  const coordinate = toLonLat(evt.coordinate);
+  m+=1;
+  if(m==1){
+    x1=floor(coordinate[0]);
+    y1=floor(coordinate[1]);
+  }
+  if(m==2){
+    x2=floor(coordinate[0]);
+    y2=floor(coordinate[1]);
+  }
+  m%=2;
+*/
+};
+
+const selectInteactionFilter = (feature) => {
+  return feature.values_.name != undefined;
+};
 </script>
 
 <template>
@@ -60,11 +100,33 @@ navigator.geolocation.getCurrentPosition(success, error, options);
       :rotation="rotation"
       :zoom="zoom"
       :projection="projection"
+      @select="featureSelected"
     />
-
-    <ol-tile-layer>
+    <ol-tile-layer
+      @select="featureSelected">
       <ol-source-osm />
     </ol-tile-layer>
+
+
+    <ol-interaction-select
+      :condition="selectCondition"
+      :filter="selectInteactionFilter"
+    >
+    </ol-interaction-select>
+
+    <ol-vector-layer>
+      <ol-source-vector>
+        <ol-feature>
+          <ol-geom-line-string :coordinates="coordinates"></ol-geom-line-string>
+          <ol-style>
+            <ol-style-stroke
+              :color="strokeColor"
+              :width="strokeWidth"
+            ></ol-style-stroke>
+          </ol-style>
+        </ol-feature>
+      </ol-source-vector>
+    </ol-vector-layer>
 
     <ol-geolocation :projection="projection" @change:position="geoLocChange">
       <ol-vector-layer :zIndex="2">
