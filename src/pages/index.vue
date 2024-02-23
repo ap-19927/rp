7e8458a89c203de7cd07b87aad7a86de8ddc7848
ap-19927 <allmannpatrick@gmail.com>
2024-02-23 08:22:34 +0000

    simple persistence

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -19,14 +19,13 @@ import Point from 'ol/geom/Point';
 import { Vector as VectorLayer } from 'ol/layer';
 import { Vector as VectorSource } from 'ol/source';
 import { Style, Circle, Fill, Stroke } from 'ol/style';
+import inventory from "public/inventory.json";
 
 const mapElement = ref("map-container");
+const formData = ref(null);
 
 onMounted(() => {
-  // Function to handle map click event
-  const handleMapClick = (event) => {
-    const coordinates = event.coordinate;
-    const [longitude, latitude] = toLonLat(coordinates);
+  const drawPlacements = (coordinates, longitude, latitude) => {
     const marker = new Feature({
       geometry: new Point(coordinates),
     });
@@ -58,6 +57,26 @@ onMounted(() => {
     overlay.setPosition(coordinates);
   }
 
+  // Function to handle map click event
+  const handleMapClick = async (event) => {
+    const coordinates = event.coordinate;
+    const [longitude, latitude] = toLonLat(coordinates);
+    const currentTime = new Date(Date.now()).toString();
+    formData.value = {
+      coordinates,
+      longitude,
+      latitude,
+      currentTime,
+    }
+
+    await useFetch("/api/upload", {
+      method: "POST",
+      body: formData,
+    });
+
+    drawPlacements(coordinates, longitude, latitude);
+  }
+
   //Initialize the map
   const map = new Map({
     target: mapElement.value,
@@ -78,6 +97,8 @@ onMounted(() => {
     stopEvent: false,
   });
   map.addOverlay(overlay);
+
+  inventory.inventory.forEach(dataPoint => drawPlacements(dataPoint.coordinates, dataPoint.longitude, dataPoint.latitude));
 
   // Event listener for map click
   map.on('click', handleMapClick);
